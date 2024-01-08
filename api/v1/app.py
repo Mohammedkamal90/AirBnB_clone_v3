20a7f6efc5138fec78ce150dd1040ce50cd355e9
Neelam <neelam_669@yahoo.com>
2024-01-08 11:03:15 -0800

    Task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module sets up a flask app and API"""
+from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from sys import getenv
+from sys import getenv 
 
 
 app = Flask(__name__)
@@ -11,13 +13,13 @@ app.register_blueprint(app_views)
 app.url_sap.strict_slashes = True
 
 
-@app.teardown_appcontext()
-def app_teardown(exception):
-    """removes the current storage session after each request"""
-    storage.close()
+@app.teardown_appcontext
+def close_storage(exception):
+        """Closes the storage on teardown."""
+        storage.close()
 
 
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+        host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+        port = int(os.environ.get('HBNB_API_PORT', 5000))
+        app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,17 @@ def api_status():
     """ returns a json response for restful api status"""
     response = ({'status': 'OK'})
     return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """ Retrieve the no. of each object by type"""
+    stats = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Places'),
+            "reviews": storage.count('Reviews'),
+            "states": storage.count('States'),
+            "users": storage.count('Users')
+            }
+    return jsonify(stats)
